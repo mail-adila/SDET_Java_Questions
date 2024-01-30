7632684f70c9f764f17387734a472d77a9ab8e0d
Adila Karim <mail.adila@gmail.com>
2024-01-29 22:02:21 -0600

    Program to check if string is palindrome

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,3 +1,38 @@
+/*
+ * Program to check if a string is a palindrome:
+ *  Write a Java program to determine if a given string is a palindrome or not. 
+*/
+
+import java.util.Scanner;
+
 public class PalindromeCheck {
+
+    public static void palindromeCheck(String input)
+    {
+        String reverse = "";
+        for(int i=input.length()-1; i>=0; i--)
+        {
+            reverse += input.charAt(i);
+        }
+        if(reverse.equals(input))
+        {
+            System.out.println("The input string is a palindrome!");
+        }
+        else
+        {
+            System.out.println("The input string is NOT palindrome!");
+        }
+    }
+
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input string:");
+        String input = sc.nextLine();
+        sc.close();
+
+        palindromeCheck(input);
+    }
     
 }
